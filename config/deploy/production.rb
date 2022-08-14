ecf78b3bc0abe875da1851f8815f3058fac0064d
Standpoint <kelivdc@yahoo.com>
2022-08-14 15:51:18 +0700

    Update config deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,9 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 server '168.138.164.218', user: 'deploy', roles: %w{app db web}
+set :ssh_options, {
+    port: 22222
+}
 
 
 # role-based syntax
